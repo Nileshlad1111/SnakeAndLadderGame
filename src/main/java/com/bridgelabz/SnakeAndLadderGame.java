71ba98b429b002b9a959b0ca529f1d9980ae1298
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-20 19:27:46 +0530

    ADDED:To proper initiation of program

--- src/main/java/com/bridgelabz/SnakeAndLadderGame.java
+++ src/main/java/com/bridgelabz/SnakeAndLadderGame.java
@@ -10,7 +10,7 @@ public class SnakeAndLadderGame {
     final static int LDDER = 2;
     final static int SNAKE = 3;
     static int USER_POSITION = START_POSITION;
-    final static int Win_Point = 100;
+    final static int WIN_POINT = 100;
 
     //two position
     static int USER1_POSITION = START_POSITION;
@@ -54,93 +54,69 @@ public class SnakeAndLadderGame {
     }
 
     //player1
-    public int player1()
-    {
-        numberOfThrownDice=numberOfThrownDice+1;
-        int numberOnDice=rollDice();
-        int option= playerOption();
-        if ( getUser1Position() < 1)
-        {
+    public int player1() {
+        numberOfThrownDice = numberOfThrownDice + 1;
+        int numberOnDice = rollDice();
+        int option = playerOption();
+        if (getUser1Position() < 1) {
             setUser1Position(START_POSITION);
         }
 
-        if (option == NUMBER_OF_PLAY)
-        {
+        if (option == NUMBER_OF_PLAY) {
             setUser1Position(getUser1Position());
-        }
-        else if (option == LDDER)
-        {
-            if (getUser1Position()+rollDice() > 100)
-            {
+        } else if (option == LDDER) {
+            if (getUser1Position() + rollDice() > 100) {
                 setUser1Position(getUser1Position());
-            }
-            else
-            {
-                setUser1Position(getUser1Position()+rollDice());
+            } else {
+                setUser1Position(getUser1Position() + rollDice());
             }
 
+        } else if (option == SNAKE) {
+            setUser1Position(getUser1Position() - rollDice());
         }
-        else if(option == SNAKE)
-        {
-            setUser1Position(getUser1Position()-rollDice());
-        }
-        System.out.println(" Times of Dice Thrown : " + numberOfThrownDice + " Position of Player 1 :" +getUser1Position());
+        System.out.println(" Times of Dice Thrown : " + numberOfThrownDice + " Position of Player 1 :" + getUser1Position());
         return USER1_POSITION;
     }
 
     //player2
-    public int player2()
-    {
-        numberOfThrownDice=numberOfThrownDice+1;
-        int numberOnDice=rollDice();
-        int option= playerOption();
-        if ( getUser2Position() < 1)
-        {
+    public int player2() {
+        numberOfThrownDice = numberOfThrownDice + 1;
+        int numberOnDice = rollDice();
+        int option = playerOption();
+        if (getUser2Position() < 1) {
             setUser2Position(START_POSITION);
         }
 
-        if (option == NUMBER_OF_PLAY)
-        {
+        if (option == NUMBER_OF_PLAY) {
             setUser2Position(getUser2Position());
-        }
-        else if (option == LDDER)
-        {
-            if (getUser2Position()+rollDice() > 100)
-            {
+        } else if (option == LDDER) {
+            if (getUser2Position() + rollDice() > 100) {
                 setUser2Position(getUser2Position());
-            }
-            else
-            {
-                setUser2Position(getUser2Position()+rollDice());
+            } else {
+                setUser2Position(getUser2Position() + rollDice());
             }
 
+        } else if (option == SNAKE) {
+            setUser2Position(getUser2Position() - rollDice());
         }
-        else if(option == SNAKE)
-        {
-            setUser2Position(getUser2Position()-rollDice());
-        }
-        System.out.println(" Times of Dice Thrown : " + numberOfThrownDice + " Position of Player 2 :" +getUser2Position());
+        System.out.println(" Times of Dice Thrown : " + numberOfThrownDice + " Position of Player 2 :" + getUser2Position());
         return USER2_POSITION;
     }
 
 
     public static void main(String[] args) {
         //object
-        SnakeAndLadderGame snakeLadder=new SnakeAndLadderGame();
-        while (true)
-        {
-            int player1Position=snakeLadder.player1();
-            int player2Position=snakeLadder.player2();
-            if (player1Position == 100)
-            {
+        SnakeAndLadderGame snakeLadder = new SnakeAndLadderGame();
+        while (true) {
+            int player1Position = snakeLadder.player1();
+            int player2Position = snakeLadder.player2();
+            if (player1Position == 100) {
                 System.out.println("player 1 Won the game ");
-                System.out.println("Total number of time thrown dice : " +numberOfThrownDice);
+                System.out.println("Total number of time thrown dice : " + numberOfThrownDice);
                 break;
-            }
-            else if (player2Position ==100)
-            {
+            } else if (player2Position == 100) {
                 System.out.println("player 2 won the game");
-                System.out.println("Total number of time thrown dice : " +numberOfThrownDice);
+                System.out.println("Total number of time thrown dice : " + numberOfThrownDice);
                 break;
             }
 
