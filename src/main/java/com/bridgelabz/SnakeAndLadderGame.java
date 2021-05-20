856f76759b30bd84433968844e9ea55852ad5df8
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-20 13:54:49 +0530

    ADDED:Given randon number 1 to 6

--- src/main/java/com/bridgelabz/SnakeAndLadderGame.java
+++ src/main/java/com/bridgelabz/SnakeAndLadderGame.java
@@ -1,12 +1,22 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadderGame {
 
-    //constant
+    //Constant
     final static int START_POSITION=0;
     final static int NUMBER_OF_PLAYER=1;
-    public static void main(String[] args) {
 
+    //Given 1 to 6 random number
+    public static int rollDice()
+    {
+        Random randomNumber=new Random();
+        int diceValue=randomNumber.nextInt(6)+1;
+        return diceValue;
+    }
+    public static void main(String[] args) {
+        System.out.println("Given random number 1 to 6 = "+rollDice());
 
     }
 }
